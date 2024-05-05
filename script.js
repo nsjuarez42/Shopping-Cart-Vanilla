52efc1d86ba2b94d7a4005ee7c3c28a0f4d740d8
Nicolas <nsjuarez42@gmail.com>
2024-05-05 12:20:31 +0200

    List products from json

--- script.js
+++ script.js
@@ -6,15 +6,68 @@ const moon = document.querySelector(".fa-solid.fa-moon")
 
 const onClickTheme = ()=>{
     if(sun.id == "hidden"){
+        document.body.className = "black"
         sun.id = ""
         moon.id = "hidden"
     }else if(moon.id == "hidden"){
+        //apply black theme to body
+        document.body.className = ""
         sun.id="hidden"
         moon.id=""
     }
 }
 sun.addEventListener("click",onClickTheme)
 moon.addEventListener("click",onClickTheme)
+
+//Create products in js
+//use this json
+/*let product_html = `<div class="">
+<img src="" alt="">
+<div class="discount"></div>
+<h3>title</h3>
+<div class="stars">
+<i class="fa-regular fa-star"></i>
+<i class="fa-regular fa-star"></i>
+<i class="fa-regular fa-star"></i>
+<i class="fa-regular fa-star"></i>
+<i class="fa-regular fa-star"></i>
+</div>
+<span>price</span>
+</div>`*/
+
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(r=>{
+    let {products} = r
+    let s = document.getElementById("products")
+    i=0;
+    while(i<9){
+        console.log('value of',i,i%3==0)
+        if(i%3==0){
+            let html = ""
+            for(let _ =0;_<3;_++){
+                console.log(_)
+
+                let {id,title,price,discountPercentage,rating,thumbnail} = products[i]
+                let stars = '<i class="fa-solid fa-star"></i>'.repeat(Math.floor(rating))
+                
+                html += `<div class="producto" id=${id}>
+                <img src="${thumbnail}" alt="">
+                <div class="discount">${discountPercentage}%</div>
+                <h3>${title}</h3>
+                <div class="stars">
+                ${stars}
+                </div>
+                <span>${price}€</span>
+                </div>`
+                i+=1
+            }
+            s.innerHTML+= `<div class="fila">${html}</div>`
+        }
+    }
+
+});
+
 
 
 
